ecd6f011fb63c8b05a9b4a9fde135f984114ed32
Winston Naidu <wnwinstonn95@gmail.com>
2026-02-23 14:03:37 +1300

    Folder structure created

--- my-profile/src/App.jsx
+++ my-profile/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Root Component, controls the layout
 function App() {
   const [count, setCount] = useState(0)
 
--- my-profile/src/main.jsx
+++ my-profile/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Entry point that mounts React into the DOM
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
