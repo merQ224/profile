ef1fb9901026b1e02c062c0cd1293023951f226a
merQ224 <wnwinstonn95@gmail.com>
2026-02-25 18:15:23 +1300

    feat: initial landing page

--- my-profile/src/pages/Landing.jsx
+++ my-profile/src/pages/Landing.jsx
@@ -1,9 +1,15 @@
+import '../styles/Landing.css'
+
 function Landing() {
     return (
-        <section>
-            <h2>Hi! I am Winston</h2>
-            <h4>Software Developer</h4>
-            <p>I am passionate about .....</p>
+        <section className='Landing'>
+            <div className='intro'>
+                <h1>Hi! I am Winston</h1>
+                <h2>Software Developer</h2>
+            </div>
+            <div className='passion'>
+                <h4>I am passionate about .....</h4>
+            </div>
         </section>
     )
 }
