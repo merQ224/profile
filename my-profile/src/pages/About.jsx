9435da6594267ef523a5f4642eb01c4caa520a21
merQ224 <wnwinstonn95@gmail.com>
2026-02-25 23:39:52 +1300

    chore: About me component layout mapped

--- my-profile/src/pages/About.jsx
+++ my-profile/src/pages/About.jsx
@@ -1,6 +1,8 @@
+import '../styles/About.css'
+
 function About() {
     return (
-        <section>
+        <section className="About">
             <h2>About Me</h2>
             <p>My professional experience</p>
         </section>
