1b6fb34541ebd433591ff66c7bd92f9b687dd899
Winston Naidu <wnwinstonn95@gmail.com>
2026-02-26 10:20:05 +1300

    chore: Career section layout

--- my-profile/src/pages/Career.jsx
+++ my-profile/src/pages/Career.jsx
@@ -1,6 +1,8 @@
+import '../styles/Career.css'
+
 function Career() {
     return (
-        <section>
+        <section className='Career'> 
             <h2>Career</h2>
             <p>My professional experience</p>
         </section>
