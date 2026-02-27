5747abe1be0be8b2405d8f45b03cc15ba0af123f
Winston Naidu <wnwinstonn95@gmail.com>
2026-02-27 19:59:18 +1300

    chore: align text on project section

---
+++ my-profile/src/data/projects.js
@@ -0,0 +1,32 @@
+export const projectData = [
+    {
+        title: "CSV Upload",
+        area: "Frontend + Backend",
+        description: "A full-stack project for uploading CSV files, processing them through a backend API, and storing the results in MongoDB. The frontend provides an interface for uploading files and viewing stored tables, while the backend handles file parsing, validation, and database operations.",
+        gif: "csv-upload.gif"
+    },
+    {
+        title: "Valentine SPA",
+        area: "Frontend",
+        description: "A funny App to send to your valentine to ask if they will be your valentine. Deployed to production via github pages.",
+        gif: "valentine.gif"
+    },
+    {
+        title: "Summarise Service Request API",
+        area: "Backend",
+        description: "This is a serverless backend powered by Node.js (TypeScript) that provides text redaction and summarisation APIs. Uses Hugging Face models for summarisation and NER (Named Entity Recognition) to detect and redact sensitive information such as names, emails, phone numbers and addresses.",
+        gif: "api-summarise-sr.gif"
+    },
+    {
+        title: "CSV Utility",
+        area: "Backend",
+        description: "Utility script for comparing CSV files, identify differences, and generate cleaned or sync outputs.",
+        gif: "csv-diff.gif"
+    },
+    {
+        title: "Angular testing",
+        area: "Frontend",
+        description: "Test different designs and behaviours.",
+        gif: "test-angular.gif"
+    }
+]
--- my-profile/src/pages/Projects.jsx
+++ my-profile/src/pages/Projects.jsx
@@ -1,4 +1,10 @@
+import '../styles/Projects.css';
+import { useRef } from 'react';
+import { projectData } from '../data/projects.js';
+
 function Projects() {
+    const scrollRef = useRef(null);
+    
     return (
         <section id="Projects">
             <h2>My Projects</h2>
